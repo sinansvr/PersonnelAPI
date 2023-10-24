d2d9984674af248c76212865b82dfb278e843b13
sinans <svrsinan06@gmail.com>
2023-10-24 15:52:28 +0300

    fixed

--- index.js
+++ index.js
@@ -27,7 +27,7 @@
 // // Syncronization (must be in commentLine):
 // // require('./src/helpers/sync')()
 
-"use strict";
+"use strict"
 
 //express
 const express = require("express");
@@ -38,19 +38,33 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
 //asyncErrors
-
 require("express-async-errors")
 
+
+//Mongo DB Connection
+const { dbConnection } = require('./src/configs/dbConnection')
+dbConnection()
+
+//JSON
 app.use(express.json());
 
+//Sessions-Cookies
+app.use(require("cookie-session")({secret:process.env.SECRET_KEY}))
+
+// res.getModelList():
+app.use(require("./src/middlewares/findSearchSortPage"))
+
+//Homepage
 app.all("/", (req, res) => {
   res.send("Welcome to PersonnelAPI!");
 });
 
+//Routes
 
 
 
-
+//errorHandler
 app.use(require("./src/middlewares/errorHandler"))
 
+//Server
 app.listen(PORT, () => console.log(`http://127.0.0.1:${PORT}`));
--- src/configs/dbConnection.js
+++ src/configs/dbConnection.js
@@ -1,37 +1,16 @@
 "use strict";
 
-const { default: mongoose } = require("mongoose");
+const mongoose = require('mongoose')
 
-/* -------------------------------------------------------
-    EXPRESS - Personnel API
-------------------------------------------------------- */
-// MongoDB Connection:
-
-// const mongoose = require('mongoose')
-
-// const dbConnection = function() {
-//     // Connect:
-//     mongoose.connect(process.env.MONGODB)
-//         .then(() => console.log('* DB Connected * '))
-//         .catch((err) => console.log('* DB Not Connected * ', err))
-// }
-
-// /* ------------------------------------------------------- */
-// module.exports = {
-//     mongoose,
-//     dbConnection
-// }
-
-const mongoose = require(mongoose);
-
-const dbConnection = () => {
-  mongoose
-    .connect(process.env.MONGODB)
-        .then(() => console.log("DB Connected"))
-        .catch((err) => console.log("DB not Connected", err))
-};
-
-module.exports={
-    dbConnection,
-    mongoose 
+const dbConnection = function() {
+   
+    mongoose.connect(process.env.MONGODB)
+        .then(() => console.log('* DB Connected * '))
+        .catch((err) => console.log('* DB Not Connected * ', err))
 }
+
+/* ------------------------------------------------------- */
+module.exports = {
+    mongoose,
+    dbConnection
+} 
